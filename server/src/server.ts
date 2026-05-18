673a95c9ebd2fc971bca9cdab9899cf6c996f3c7
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2026-05-18 08:27:16 -0700

    Switching to tsx instead of node-ts to handle esModule smoothly without any configurations and cleaner outputs under the hood

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,13 @@ import type { Express } from 'express'
 
 const app: Express = express();
 
-app.use(express())
+app.use(express.json())
 
 const PORT = 8000;
+
+//https://localhost:8000
+app.get('/', (req, res) => {
+    res.json({message: "hi"})
+})
 
 app.listen(PORT, ():void => console.log(`Listening on port ${PORT}`))
